eb3a605915352d89c45244960a9836b59720e6b3
Ewan-desktop <chl8263@naver.com>
2023-12-04 22:09:23 +0900

    init

---
+++ src/main/java/com/example/dddpractices/DddPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddPracticesApplication.class, args);
+    }
+
+}
